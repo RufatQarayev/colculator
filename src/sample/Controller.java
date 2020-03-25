bc324528d5d01c5af50db007d2f10d38f5b64f74
Rufat Garayev <karaev.rufat@gmail.com>
2020-03-26 00:57:44 +0400

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+import java.awt.*;
+
+public class  Controller {
+    public Label helloWorld;
+
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
+}
